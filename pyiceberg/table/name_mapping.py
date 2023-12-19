199fb8571ccd2b5f7356debdde79864a2bf38199
Fokko Driesprong <fokko@tabular.io>
2023-12-19 01:53:20 +0100

    Add name-mapping (#212)

--- pyiceberg/table/name_mapping.py
+++ pyiceberg/table/name_mapping.py
@@ -27,21 +27,22 @@ from collections import ChainMap
 from functools import cached_property, singledispatch
 from typing import Any, Dict, Generic, List, TypeVar, Union
 
-from pydantic import Field, conlist, model_serializer
+from pydantic import Field, conlist, field_validator, model_serializer
 
 from pyiceberg.schema import Schema, SchemaVisitor, visit
 from pyiceberg.typedef import IcebergBaseModel, IcebergRootModel
-from pyiceberg.types import IcebergType, ListType, MapType, NestedField, PrimitiveType, StructType
-
-LIST_ELEMENT_NAME = "element"
-MAP_KEY_NAME = "key"
-MAP_VALUE_NAME = "value"
+from pyiceberg.types import ListType, MapType, NestedField, PrimitiveType, StructType
 
 
 class MappedField(IcebergBaseModel):
     field_id: int = Field(alias="field-id")
     names: List[str] = conlist(str, min_length=1)
     fields: List[MappedField] = Field(default_factory=list)
+
+    @field_validator('fields', mode='before')
+    @classmethod
+    def convert_null_to_empty_List(cls, v: Any) -> Any:
+        return v or []
 
     @model_serializer
     def ser_model(self) -> Dict[str, Any]:
@@ -69,24 +70,15 @@ class NameMapping(IcebergRootModel[List[MappedField]]):
     root: List[MappedField]
 
     @cached_property
-    def _field_by_id(self) -> Dict[int, MappedField]:
-        return visit_name_mapping(self, _IndexById())
-
-    @cached_property
     def _field_by_name(self) -> Dict[str, MappedField]:
         return visit_name_mapping(self, _IndexByName())
 
-    def id(self, name: str) -> int:
+    def find(self, *names: str) -> MappedField:
+        name = '.'.join(names)
         try:
-            return self._field_by_name[name].field_id
+            return self._field_by_name[name]
         except KeyError as e:
             raise ValueError(f"Could not find field with name: {name}") from e
-
-    def field(self, field_id: int) -> MappedField:
-        try:
-            return self._field_by_id[field_id]
-        except KeyError as e:
-            raise ValueError(f"Could not find field-id: {field_id}") from e
 
     def __len__(self) -> int:
         """Return the number of mappings."""
@@ -115,27 +107,6 @@ class NameMappingVisitor(Generic[T], ABC):
     @abstractmethod
     def field(self, field: MappedField, field_result: T) -> T:
         """Visit a MappedField."""
-
-
-class _IndexById(NameMappingVisitor[Dict[int, MappedField]]):
-    result: Dict[int, MappedField]
-
-    def __init__(self) -> None:
-        self.result = {}
-
-    def mapping(self, nm: NameMapping, field_results: Dict[int, MappedField]) -> Dict[int, MappedField]:
-        return field_results
-
-    def fields(self, struct: List[MappedField], field_results: List[Dict[int, MappedField]]) -> Dict[int, MappedField]:
-        return self.result
-
-    def field(self, field: MappedField, field_result: Dict[int, MappedField]) -> Dict[int, MappedField]:
-        if field.field_id in self.result:
-            raise ValueError(f"Invalid mapping: ID {field.field_id} is not unique")
-
-        self.result[field.field_id] = field
-
-        return self.result
 
 
 class _IndexByName(NameMappingVisitor[Dict[str, MappedField]]):
@@ -173,7 +144,7 @@ def _(fields: List[MappedField], visitor: NameMappingVisitor[T]) -> T:
     return visitor.fields(fields, results)
 
 
-def load_mapping_from_json(mapping: str) -> NameMapping:
+def parse_mapping_from_json(mapping: str) -> NameMapping:
     return NameMapping.model_validate_json(mapping)
 
 
@@ -183,7 +154,7 @@ class _CreateMapping(SchemaVisitor[List[MappedField]]):
 
     def struct(self, struct: StructType, field_results: List[List[MappedField]]) -> List[MappedField]:
         return [
-            MappedField(field_id=field.field_id, names={field.name}, fields=result)
+            MappedField(field_id=field.field_id, names=[field.name], fields=result)
             for field, result in zip(struct.fields, field_results)
         ]
 
@@ -191,12 +162,12 @@ class _CreateMapping(SchemaVisitor[List[MappedField]]):
         return field_result
 
     def list(self, list_type: ListType, element_result: List[MappedField]) -> List[MappedField]:
-        return [MappedField(field_id=list_type.element_id, names={"element"}, fields=element_result)]
+        return [MappedField(field_id=list_type.element_id, names=["element"], fields=element_result)]
 
     def map(self, map_type: MapType, key_result: List[MappedField], value_result: List[MappedField]) -> List[MappedField]:
         return [
-            MappedField(field_id=map_type.key_id, names={"key"}, fields=key_result),
-            MappedField(field_id=map_type.value_id, names={"value"}, fields=value_result),
+            MappedField(field_id=map_type.key_id, names=["key"], fields=key_result),
+            MappedField(field_id=map_type.value_id, names=["value"], fields=value_result),
         ]
 
     def primitive(self, primitive: PrimitiveType) -> List[MappedField]:
