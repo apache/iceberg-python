a276112e12a481c5f461a46571b4554b74a67586
Kevin Liu <kevinjqliu@users.noreply.github.com>
2024-02-04 13:41:45 -0800

    sql-catalog: Default `create_engine` echo to `False` (#360)
    * default create_engine echo to False
    
    * Update pyiceberg/catalog/sql.py
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    ---------
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- pyiceberg/catalog/sql.py
+++ pyiceberg/catalog/sql.py
@@ -101,7 +101,8 @@ class SqlCatalog(Catalog):
 
         if not (uri_prop := self.properties.get("uri")):
             raise NoSuchPropertyException("SQL connection URI is required")
-        self.engine = create_engine(uri_prop, echo=True)
+        echo = bool(self.properties.get("echo", False))
+        self.engine = create_engine(uri_prop, echo=echo)
 
         self._ensure_tables_exist()
 
