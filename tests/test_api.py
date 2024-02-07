0ac5f9eb9209c560108540a09196003aa18dd6d3
Sung Yun <107272191+syun64@users.noreply.github.com>
2024-02-07 22:03:34 +0000

    detect breaking changes

---
+++ tests/test_api.py
@@ -0,0 +1,41 @@
+import re
+import subprocess
+
+import griffe
+from griffe.exceptions import GitError
+
+
+def test_breaking_change() -> None:
+    fetch_process = subprocess.run(
+        ["git", "fetch", "--tags", "upstream"],
+        check=False,
+    )
+    if fetch_process.returncode != 0:
+        raise GitError("Cannot fetch Git tags from upstream")
+
+    tag_process = subprocess.run(
+        ["git", "tag", "-l", "--sort=-version:refname"],
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        check=False,
+        encoding='utf-8',
+    )
+    output = tag_process.stdout.strip()
+    if tag_process.returncode != 0 or not output:
+        raise GitError(f"Cannot list Git tags from upstream: {output or 'no tags'}")
+    tags = output.split("\n")
+
+    p = re.compile(r"^pyiceberg-\d+.\d+.\d+$")
+
+    releases = [t for t in tags if p.match(t)]
+
+    previous = griffe.load_git("pyiceberg", ref=releases[0])
+    current = griffe.load_git("pyiceberg")
+
+    breaking_changes = []
+
+    for breakage in griffe.find_breaking_changes(previous, current):
+        breaking_changes.append(breakage.as_dict())
+
+    assert not breaking_changes
