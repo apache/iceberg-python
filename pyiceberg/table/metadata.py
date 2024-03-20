ad840d54f87d2bdb28a964afc046fde74ce50da2
HonahX <undefined.newdb.newtable@gmail.com>
2024-03-20 01:34:24 -0700

    remove extra line

--- pyiceberg/table/metadata.py
+++ pyiceberg/table/metadata.py
@@ -458,8 +458,6 @@ class TableMetadataV2(TableMetadataCommonFields, IcebergBaseModel):
 
 TableMetadata = Annotated[Union[TableMetadataV1, TableMetadataV2], Field(discriminator="format_version")]
 
-EMPTY_METADATA = TableMetadataV1(location="", schema=Schema(), last_column_id=-1, partition_spec=[])
-
 
 def new_table_metadata(
     schema: Schema,
