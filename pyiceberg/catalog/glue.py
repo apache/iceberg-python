eed16e023dcfe2a1c9edf455c76e6d1b3073caf5
HonahX <honahx@apache.org>
2024-04-24 22:03:06 -0700

    extract common logic of _commit_table

--- pyiceberg/catalog/glue.py
+++ pyiceberg/catalog/glue.py
@@ -67,7 +67,6 @@ from pyiceberg.table import (
     CommitTableResponse,
     PropertyUtil,
     Table,
-    update_table_metadata,
 )
 from pyiceberg.table.metadata import TableMetadata
 from pyiceberg.table.sorting import UNSORTED_SORT_ORDER, SortOrder
@@ -451,25 +450,16 @@ class GlueCatalog(MetastoreCatalog):
             glue_table_version_id = None
             current_table = None
 
-        for requirement in table_request.requirements:
-            requirement.validate(current_table.metadata if current_table else None)
+        updated_staged_table = self._update_and_stage_table(current_table, table_request)
 
-        updated_metadata = update_table_metadata(
-            base_metadata=current_table.metadata if current_table else self._empty_table_metadata(),
-            updates=table_request.updates,
-            enforce_validation=current_table is None,
-        )
-
-        if current_table and updated_metadata == current_table.metadata:
+        if current_table and updated_staged_table.metadata == current_table.metadata:
             # no changes, do nothing
             return CommitTableResponse(metadata=current_table.metadata, metadata_location=current_table.metadata_location)
 
-        new_metadata_version = self._parse_metadata_version(current_table.metadata_location) + 1 if current_table else 0
-        new_metadata_location = self._get_metadata_location(updated_metadata.location, new_metadata_version)
         self._write_metadata(
-            metadata=updated_metadata,
-            io=self._load_file_io(updated_metadata.properties, new_metadata_location),
-            metadata_path=new_metadata_location,
+            metadata=updated_staged_table.metadata,
+            io=updated_staged_table.io,
+            metadata_path=updated_staged_table.metadata_location,
         )
 
         if current_table:
@@ -483,9 +473,9 @@ class GlueCatalog(MetastoreCatalog):
             # modifications occur. See more details at https://iceberg.apache.org/docs/latest/aws/#optimistic-locking
             update_table_input = _construct_table_input(
                 table_name=table_name,
-                metadata_location=new_metadata_location,
-                properties=updated_metadata.properties,
-                metadata=updated_metadata,
+                metadata_location=updated_staged_table.metadata_location,
+                properties=updated_staged_table.properties,
+                metadata=updated_staged_table.metadata,
                 glue_table=current_glue_table,
                 prev_metadata_location=current_table.metadata_location,
             )
@@ -499,13 +489,15 @@ class GlueCatalog(MetastoreCatalog):
             # table does not exist, create the table
             create_table_input = _construct_table_input(
                 table_name=table_name,
-                metadata_location=new_metadata_location,
-                properties=updated_metadata.properties,
-                metadata=updated_metadata,
+                metadata_location=updated_staged_table.metadata_location,
+                properties=updated_staged_table.properties,
+                metadata=updated_staged_table.metadata,
             )
             self._create_glue_table(database_name=database_name, table_name=table_name, table_input=create_table_input)
 
-        return CommitTableResponse(metadata=updated_metadata, metadata_location=new_metadata_location)
+        return CommitTableResponse(
+            metadata=updated_staged_table.metadata, metadata_location=updated_staged_table.metadata_location
+        )
 
     def load_table(self, identifier: Union[str, Identifier]) -> Table:
         """Load the table's metadata and returns the table instance.
