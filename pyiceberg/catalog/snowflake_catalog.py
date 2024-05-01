87d8548b3b6394d2ccbc38b9ff1b2d791bd95909
Prabodh Agarwal <prabodh1194@gmail.com>
2024-05-01 14:54:01 +0530

    define snowflake catalog

---
+++ pyiceberg/catalog/snowflake_catalog.py
@@ -0,0 +1,221 @@
+from __future__ import annotations
+
+import json
+import os
+from dataclasses import dataclass
+from typing import Iterator, List, Optional, Set, Union
+
+import pyarrow as pa
+from boto3.session import Session
+from snowflake.connector import DictCursor, SnowflakeConnection
+
+from pyiceberg.catalog import MetastoreCatalog, PropertiesUpdateSummary
+from pyiceberg.exceptions import NoSuchTableError, TableAlreadyExistsError
+from pyiceberg.io import S3_ACCESS_KEY_ID, S3_REGION, S3_SECRET_ACCESS_KEY, S3_SESSION_TOKEN
+from pyiceberg.partitioning import UNPARTITIONED_PARTITION_SPEC, PartitionSpec
+from pyiceberg.schema import Schema
+from pyiceberg.table import CommitTableRequest, CommitTableResponse, StaticTable, Table, sorting
+from pyiceberg.typedef import EMPTY_DICT, Identifier, Properties
+
+
+class SnowflakeCatalog(MetastoreCatalog):
+    @dataclass(frozen=True, eq=True)
+    class _SnowflakeIdentifier:
+        database: str | None
+        schema: str | None
+        table: str | None
+
+        def __iter__(self) -> Iterator[str]:
+            """
+            Iterate of the non-None parts of the identifier.
+
+            Returns:
+                Iterator[str]: Iterator of the non-None parts of the identifier.
+            """
+            yield from filter(None, [self.database, self.schema, self.table])
+
+        @classmethod
+        def table_from_string(cls, identifier: str) -> SnowflakeCatalog._SnowflakeIdentifier:
+            parts = identifier.split(".")
+            if len(parts) == 1:
+                return cls(None, None, parts[0])
+            elif len(parts) == 2:
+                return cls(None, parts[0], parts[1])
+            elif len(parts) == 3:
+                return cls(parts[0], parts[1], parts[2])
+
+            raise ValueError(f"Invalid identifier: {identifier}")
+
+        @classmethod
+        def schema_from_string(cls, identifier: str) -> SnowflakeCatalog._SnowflakeIdentifier:
+            parts = identifier.split(".")
+            if len(parts) == 1:
+                return cls(None, parts[0], None)
+            elif len(parts) == 2:
+                return cls(parts[0], parts[1], None)
+
+            raise ValueError(f"Invalid identifier: {identifier}")
+
+        @property
+        def table_name(self) -> str:
+            return ".".join(self)
+
+        @property
+        def schema_name(self) -> str:
+            return ".".join(self)
+
+    def __init__(self, name: str, **properties: str):
+        super().__init__(name, **properties)
+
+        params = {
+            "user": properties["user"],
+            "account": properties["account"],
+        }
+
+        if "authenticator" in properties:
+            params["authenticator"] = properties["authenticator"]
+
+        if "password" in properties:
+            params["password"] = properties["password"]
+
+        if "private_key" in properties:
+            params["private_key"] = properties["private_key"]
+
+        self.connection = SnowflakeConnection(**params)
+
+    def load_table(self, identifier: Union[str, Identifier]) -> Table:
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.table_from_string(
+            identifier if isinstance(identifier, str) else ".".join(identifier)
+        )
+
+        metadata_query = "SELECT SYSTEM$GET_ICEBERG_TABLE_INFORMATION(%s) AS METADATA"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            try:
+                cursor.execute(metadata_query, (sf_identifier.table_name,))
+                metadata = json.loads(cursor.fetchone()["METADATA"])["metadataLocation"]
+            except Exception as e:
+                raise NoSuchTableError(f"Table {sf_identifier.table_name} not found") from e
+
+        session = Session()
+        credentials = session.get_credentials()
+        current_credentials = credentials.get_frozen_credentials()
+
+        s3_props = {
+            S3_ACCESS_KEY_ID: current_credentials.access_key,
+            S3_SECRET_ACCESS_KEY: current_credentials.secret_key,
+            S3_SESSION_TOKEN: current_credentials.token,
+            S3_REGION: os.environ.get("AWS_REGION", "us-east-1"),
+        }
+
+        tbl = StaticTable.from_metadata(metadata, properties=s3_props)
+        tbl.identifier = tuple(identifier.split(".")) if isinstance(identifier, str) else identifier
+        tbl.catalog = self
+
+        return tbl
+
+    def register_table(self, identifier: Union[str, Identifier], metadata_location: str) -> Table:
+        query = "CREATE ICEBERG TABLE (%s) METADATA_FILE_PATH = (%s)"
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.table_from_string(
+            identifier if isinstance(identifier, str) else ".".join(identifier)
+        )
+
+        with self.connection.cursor(DictCursor) as cursor:
+            try:
+                cursor.execute(query, (sf_identifier.table_name, metadata_location))
+            except Exception as e:
+                raise TableAlreadyExistsError(f"Table {sf_identifier.table_name} already exists") from e
+
+        return self.load_table(identifier)
+
+    def drop_table(self, identifier: Union[str, Identifier]) -> None:
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.table_from_string(
+            identifier if isinstance(identifier, str) else ".".join(identifier)
+        )
+
+        query = "DROP TABLE IF EXISTS (%s)"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            cursor.execute(query, (sf_identifier.table_name,))
+
+    def rename_table(self, from_identifier: Union[str, Identifier], to_identifier: Union[str, Identifier]) -> Table:
+        sf_from_identifier = SnowflakeCatalog._SnowflakeIdentifier.table_from_string(
+            from_identifier if isinstance(from_identifier, str) else ".".join(from_identifier)
+        )
+        sf_to_identifier = SnowflakeCatalog._SnowflakeIdentifier.table_from_string(
+            to_identifier if isinstance(to_identifier, str) else ".".join(to_identifier)
+        )
+
+        query = "ALTER TABLE (%s) RENAME TO (%s)"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            cursor.execute(query, (sf_from_identifier.table_name, sf_to_identifier.table_name))
+
+        return self.load_table(to_identifier)
+
+    def _commit_table(self, table_request: CommitTableRequest) -> CommitTableResponse:
+        raise NotImplementedError
+
+    def create_namespace(self, namespace: Union[str, Identifier], properties: Properties = EMPTY_DICT) -> None:
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.schema_from_string(
+            namespace if isinstance(namespace, str) else ".".join(namespace)
+        )
+
+        db_query = "CREATE DATABASE IF NOT EXISTS (%s)"
+        schema_query = "CREATE SCHEMA IF NOT EXISTS (%s)"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            if sf_identifier.database:
+                cursor.execute(db_query, (sf_identifier.database,))
+            cursor.execute(schema_query, (sf_identifier.schema_name,))
+
+    def drop_namespace(self, namespace: Union[str, Identifier]) -> None:
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.schema_from_string(
+            namespace if isinstance(namespace, str) else ".".join(namespace)
+        )
+
+        sf_query = "DROP SCHEMA IF EXISTS (%s)"
+        db_query = "DROP DATABASE IF EXISTS (%s)"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            if sf_identifier.database:
+                cursor.execute(db_query, (sf_identifier.database,))
+            cursor.execute(sf_query, (sf_identifier.schema_name,))
+
+    def list_tables(self, namespace: Union[str, Identifier]) -> List[Identifier]:
+        sf_identifier = SnowflakeCatalog._SnowflakeIdentifier.schema_from_string(
+            namespace if isinstance(namespace, str) else ".".join(namespace)
+        )
+
+        schema_query = "SHOW ICEBERG TABLES IN SCHEMA (%s)"
+        db_query = "SHOW ICEBERG TABLES IN DATABASE (%s)"
+
+        with self.connection.cursor(DictCursor) as cursor:
+            if sf_identifier.database:
+                cursor.execute(db_query, (sf_identifier.database,))
+            else:
+                cursor.execute(schema_query, (sf_identifier.schema,))
+
+            return [(row["database_name"], row["schema_name"], row["table_name"]) for row in cursor.fetchall()]
+
+    def list_namespaces(self, namespace: Union[str, Identifier] = ()) -> List[Identifier]:
+        raise NotImplementedError
+
+    def load_namespace_properties(self, namespace: Union[str, Identifier]) -> Properties:
+        raise NotImplementedError
+
+    def update_namespace_properties(
+        self, namespace: Union[str, Identifier], removals: Optional[Set[str]] = None, updates: Properties = EMPTY_DICT
+    ) -> PropertiesUpdateSummary:
+        raise NotImplementedError
+
+    def create_table(
+        self,
+        identifier: Union[str, Identifier],
+        schema: Union[Schema, pa.Schema],
+        location: Optional[str] = None,
+        partition_spec: PartitionSpec = UNPARTITIONED_PARTITION_SPEC,
+        sort_order: sorting.SortOrder = sorting.UNSORTED_SORT_ORDER,
+        properties: Properties = EMPTY_DICT,
+    ) -> Table:
+        raise NotImplementedError
---
+++ tests/catalog/test_snowflake_catalog.py
@@ -0,0 +1,229 @@
+import json
+from typing import Any, Generator, List
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from pyiceberg.catalog.snowflake_catalog import SnowflakeCatalog
+from pyiceberg.table.metadata import TableMetadataUtil
+
+
+class TestSnowflakeIdentifier:
+    def test_get_table_name(self) -> None:
+        sf_id = SnowflakeCatalog._SnowflakeIdentifier.table_from_string("db.schema.table")
+        assert sf_id.table_name == "db.schema.table"
+
+        sf_id = SnowflakeCatalog._SnowflakeIdentifier.table_from_string("schema.table")
+        assert sf_id.table_name == "schema.table"
+
+        sf_id = SnowflakeCatalog._SnowflakeIdentifier.table_from_string("table")
+        assert sf_id.table_name == "table"
+
+        with pytest.raises(ValueError):
+            SnowflakeCatalog._SnowflakeIdentifier.table_from_string("db.schema.table.extra")
+
+    def test_get_schema_name(self) -> None:
+        sf_id = SnowflakeCatalog._SnowflakeIdentifier.schema_from_string("db.schema")
+        assert sf_id.schema_name == "db.schema"
+
+        sf_id = SnowflakeCatalog._SnowflakeIdentifier.schema_from_string("schema")
+        assert sf_id.schema_name == "schema"
+
+        with pytest.raises(ValueError):
+            SnowflakeCatalog._SnowflakeIdentifier.schema_from_string("db.schema.extra")
+
+
+class MockSnowflakeCursor:
+    q = ""
+    qs: List[Any] = []
+
+    def __enter__(self) -> Any:
+        return self
+
+    def __exit__(self, *args: Any) -> None:
+        pass
+
+    def rollback(self) -> None:
+        pass
+
+    def fetchall(self) -> Any:
+        if "SHOW ICEBERG TABLES" in self.q:
+            return [
+                {
+                    "database_name": "db",
+                    "schema_name": "schema",
+                    "table_name": "tbl_1",
+                },
+                {
+                    "database_name": "db",
+                    "schema_name": "schema",
+                    "table_name": "tbl_2",
+                },
+            ]
+
+        return []
+
+    def fetchone(self) -> Any:
+        if "SYSTEM$GET_ICEBERG_TABLE_INFORMATION" in self.q:
+            return {
+                "METADATA": json.dumps({
+                    "metadataLocation": "s3://bucket/path/to/metadata.json",
+                })
+            }
+
+    def execute(self, *args: Any, **kwargs: Any) -> Any:
+        self.q = args[0]
+        self.qs.append(args)
+
+
+class MockSnowflakeConnection:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self._cursor = MockSnowflakeCursor()
+        self._cursor.qs = []
+
+    def cursor(self, *args: Any, **kwargs: Any) -> Any:
+        return self._cursor
+
+
+class MockCreds:
+    def get_frozen_credentials(self) -> Any:
+        creds = MagicMock()
+
+        creds.access_key = ""
+        creds.secret_key = ""
+        creds.token = ""
+
+        return creds
+
+
+class TestSnowflakeCatalog:
+    @pytest.fixture(scope="function")
+    def snowflake_catalog(self) -> Generator[SnowflakeCatalog, None, None]:
+        with patch(
+            "pyiceberg.serializers.FromInputFile.table_metadata",
+            return_value=TableMetadataUtil.parse_obj({
+                "format-version": 2,
+                "location": "s3://bucket/path/to/",
+                "last-column-id": 4,
+                "schemas": [{}],
+                "partition-specs": [{}],
+            }),
+        ):
+            with patch("pyiceberg.catalog.snowflake_catalog.Session.get_credentials", MockCreds):
+                with patch("pyiceberg.catalog.snowflake_catalog.SnowflakeConnection", MockSnowflakeConnection):
+                    yield SnowflakeCatalog(
+                        name="test",
+                        user="",
+                        account="",
+                    )
+
+    def test_load_table(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        tbl = snowflake_catalog.load_table("db.schema.table")
+
+        assert tbl is not None
+
+    def test_register_table(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        qs = snowflake_catalog.connection._cursor.qs
+
+        tbl = snowflake_catalog.register_table("db.schema.table", "s3://bucket/path/to/metadata.json")
+
+        assert len(qs) == 2
+
+        assert qs[0][0] == "CREATE ICEBERG TABLE (%s) METADATA_FILE_PATH = (%s)"
+        assert qs[0][1] == ("db.schema.table", "s3://bucket/path/to/metadata.json")
+
+        assert tbl is not None
+
+    def test_drop_table(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.drop_table("db.schema.table")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 1
+
+        assert qs[0][0] == "DROP TABLE IF EXISTS (%s)"
+        assert qs[0][1] == ("db.schema.table",)
+
+    def test_rename_table(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.rename_table("table", "schema.new_table")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 2
+
+        assert qs[0][0] == "ALTER TABLE (%s) RENAME TO (%s)"
+        assert qs[0][1] == ("table", "schema.new_table")
+
+    def test_create_namespace_schema_only(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.create_namespace("schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 1
+
+        assert qs[0][0] == "CREATE SCHEMA IF NOT EXISTS (%s)"
+        assert qs[0][1] == ("schema",)
+
+    def test_create_namespace_with_db(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.create_namespace("db.schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 2
+
+        assert qs[0][0] == "CREATE DATABASE IF NOT EXISTS (%s)"
+        assert qs[0][1] == ("db",)
+
+        assert qs[1][0] == "CREATE SCHEMA IF NOT EXISTS (%s)"
+        assert qs[1][1] == ("db.schema",)
+
+    def test_drop_namespace_schema_only(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.drop_namespace("schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 1
+
+        assert qs[0][0] == "DROP SCHEMA IF EXISTS (%s)"
+        assert qs[0][1] == ("schema",)
+
+    def test_drop_namespace_with_db(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        snowflake_catalog.drop_namespace("db.schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 2
+
+        assert qs[0][0] == "DROP DATABASE IF EXISTS (%s)"
+        assert qs[0][1] == ("db",)
+
+        assert qs[1][0] == "DROP SCHEMA IF EXISTS (%s)"
+        assert qs[1][1] == ("db.schema",)
+
+    def test_list_tables_schema_only(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        tabs = snowflake_catalog.list_tables("schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 1
+
+        assert qs[0][0] == "SHOW ICEBERG TABLES IN SCHEMA (%s)"
+        assert qs[0][1] == ("schema",)
+
+        assert len(tabs) == 2
+        assert tabs[0] == ("db", "schema", "tbl_1")
+        assert tabs[1] == ("db", "schema", "tbl_2")
+
+    def test_list_tables_with_db(self, snowflake_catalog: SnowflakeCatalog) -> None:
+        tabs = snowflake_catalog.list_tables("db.schema")
+
+        qs = snowflake_catalog.connection._cursor.qs
+
+        assert len(qs) == 1
+
+        assert qs[0][0] == "SHOW ICEBERG TABLES IN DATABASE (%s)"
+        assert qs[0][1] == ("db",)
+
+        assert len(tabs) == 2
+        assert tabs[0] == ("db", "schema", "tbl_1")
+        assert tabs[1] == ("db", "schema", "tbl_2")
