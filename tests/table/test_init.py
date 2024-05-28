ebe0b1d5c5cfa90d0c84a44b03ba2d58e64910a7
chinmay-bhat <12948588+chinmay-bhat@users.noreply.github.com>
2024-05-28 13:54:23 +0530

    update tests

--- tests/table/test_init.py
+++ tests/table/test_init.py
@@ -205,7 +205,7 @@ def test_snapshot_by_id(table_v2: Table) -> None:
 
 
 def test_snapshot_by_timestamp(table_v2: Table) -> None:
-    assert table_v2.latest_snapshot_before_timestamp(1555100955771) == Snapshot(
+    assert table_v2.snapshot_at_or_before_timestamp(1555100955771) == Snapshot(
         snapshot_id=3055729675574597004,
         parent_snapshot_id=3051729675574597004,
         sequence_number=1,
@@ -217,7 +217,7 @@ def test_snapshot_by_timestamp(table_v2: Table) -> None:
 
 
 def test_current_ancestors(table_v2: Table) -> None:
-    assert table_v2.current_ancestors() == [
+    assert list(table_v2.current_ancestors()) == [
         Snapshot(
             snapshot_id=3055729675574597004,
             parent_snapshot_id=3051729675574597004,
