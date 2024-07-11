ff654fb5045b81cdc782f88c7391953aa0d56233
Sung Yun <107272191+syun64@users.noreply.github.com>
2024-07-11 13:52:40 +0000

    lint

--- tests/integration/test_writes/test_writes.py
+++ tests/integration/test_writes/test_writes.py
@@ -968,7 +968,9 @@ def table_write_subset_of_schema(session_catalog: Catalog, arrow_table_with_null
 
 @pytest.mark.integration
 @pytest.mark.parametrize("format_version", [1, 2])
-def test_write_all_timestamp_precision(mocker: MockerFixture, spark: SparkSession, session_catalog: Catalog, format_version: int) -> None:
+def test_write_all_timestamp_precision(
+    mocker: MockerFixture, spark: SparkSession, session_catalog: Catalog, format_version: int
+) -> None:
     identifier = "default.table_all_timestamp_precision"
     arrow_table_schema_with_all_timestamp_precisions = pa.schema([
         ("timestamp_s", pa.timestamp(unit="s")),
@@ -981,7 +983,6 @@ def test_write_all_timestamp_precision(mocker: MockerFixture, spark: SparkSessio
         ("timestamptz_ns", pa.timestamp(unit="ns", tz="UTC")),
         ("timestamptz_us_etc_utc", pa.timestamp(unit="us", tz="Etc/UTC")),
         ("timestamptz_us_z", pa.timestamp(unit="us", tz="Z")),
-        
     ])
     TEST_DATA_WITH_NULL = {
         "timestamp_s": [datetime(2023, 1, 1, 19, 25, 00), None, datetime(2023, 3, 1, 19, 25, 00)],
@@ -1051,6 +1052,7 @@ def test_write_all_timestamp_precision(mocker: MockerFixture, spark: SparkSessio
     rhs = written_arrow_table.to_pandas()
     print(rhs.dtypes)
     assert lhs.equals(rhs)
+
 
 @pytest.mark.integration
 @pytest.mark.parametrize("format_version", [1, 2])
