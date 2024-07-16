05740ff12de565215001410d87d6393a6e51cd5c
Adrian Qin <147659252+jqin61@users.noreply.github.com>
2024-07-16 17:51:35 +0000

    remove arrow df cast

--- pyiceberg/table/__init__.py
+++ pyiceberg/table/__init__.py
@@ -543,11 +543,6 @@ class Transaction:
 
         _check_schema_compatible(self._table.schema(), other_schema=df.schema)
 
-        # cast if the two schemas are compatible but not equal
-        table_arrow_schema = self._table.schema().as_arrow()
-        if table_arrow_schema != df.schema:
-            df = df.cast(table_arrow_schema)
-
         # If dataframe does not have data, there is no need to overwrite
         if df.shape[0] == 0:
             return
