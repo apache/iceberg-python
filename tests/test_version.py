81b7ed833fd9a9e05510d17d839a6ae00fac5672
laksh-krishna-sharma <asklakshsharma@gmail.com>
2024-08-09 17:20:38 +0530

    linter fix

--- tests/test_version.py
+++ tests/test_version.py
@@ -23,9 +23,7 @@ def test_version_format() -> None:
 
     installed_version = metadata.version("pyiceberg")
 
-    assert (
-        __version__ == installed_version
-    ), (
+    assert __version__ == installed_version, (
         f"The installed version ({installed_version}) does not match the current codebase version ({__version__})."
         "This failure could be due to a recent version bump in the Pyiceberg library. "
         "Please ensure you have the latest version installed by rerunning `make install` command."
