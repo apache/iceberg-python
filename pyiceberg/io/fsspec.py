3b31d2f2e3bcade07ad9ba613205d006a88a5588
Guilherme Torres Castro <1149991+guitcastro@users.noreply.github.com>
2024-08-12 12:09:27 -0300

    Support `s3.signer.endpoint` for the REST catalog (#1029)
    * s3_signer_endpoint
    
    * prune any trailing whitespaces
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * fallback to default value instead of "endpoint" property
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * fix test_s3v4_rest_signer_endpoint
    
    * Fix missing backtick
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * rename S3_SIGNER_ENDPOINT_DEFAULT_VALUE to S3_SIGNER_ENDPOINT_DEFAULT
    
    * fix s3.signer.endpoint docs
    
    * fk typo in signer
    
    * fix fmt
    
    ---------
    
    Co-authored-by: guilhermecastro <guilherme.castro@protonmail.com>
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- pyiceberg/io/fsspec.py
+++ pyiceberg/io/fsspec.py
@@ -67,6 +67,8 @@ from pyiceberg.io import (
     S3_REGION,
     S3_SECRET_ACCESS_KEY,
     S3_SESSION_TOKEN,
+    S3_SIGNER_ENDPOINT,
+    S3_SIGNER_ENDPOINT_DEFAULT,
     S3_SIGNER_URI,
     ADLFS_ClIENT_SECRET,
     FileIO,
@@ -86,6 +88,8 @@ def s3v4_rest_signer(properties: Properties, request: AWSRequest, **_: Any) -> A
         raise SignError("Signer set, but token is not available")
 
     signer_url = properties.get(S3_SIGNER_URI, properties["uri"]).rstrip("/")
+    signer_endpoint = properties.get(S3_SIGNER_ENDPOINT, S3_SIGNER_ENDPOINT_DEFAULT)
+
     signer_headers = {"Authorization": f"Bearer {properties[TOKEN]}"}
     signer_body = {
         "method": request.method,
@@ -94,7 +98,7 @@ def s3v4_rest_signer(properties: Properties, request: AWSRequest, **_: Any) -> A
         "headers": {key: [val] for key, val in request.headers.items()},
     }
 
-    response = requests.post(f"{signer_url}/v1/aws/s3/sign", headers=signer_headers, json=signer_body)
+    response = requests.post(f"{signer_url}/{signer_endpoint.strip()}", headers=signer_headers, json=signer_body)
     try:
         response.raise_for_status()
         response_json = response.json()
@@ -131,9 +135,9 @@ def _s3(properties: Properties) -> AbstractFileSystem:
 
     if signer := properties.get("s3.signer"):
         logger.info("Loading signer %s", signer)
-        if singer_func := SIGNERS.get(signer):
-            singer_func_with_properties = partial(singer_func, properties)
-            register_events["before-sign.s3"] = singer_func_with_properties
+        if signer_func := SIGNERS.get(signer):
+            signer_func_with_properties = partial(signer_func, properties)
+            register_events["before-sign.s3"] = signer_func_with_properties
 
             # Disable the AWS Signer
             config_kwargs["signature_version"] = UNSIGNED
