ea68f8844f21f884a3d5dbfca78b6f180d2e6087
Shiv Gupta <shiv.gupta@chainalysis.com>
2024-11-07 18:52:36 -0500

    Lint

--- tests/catalog/test_rest.py
+++ tests/catalog/test_rest.py
@@ -461,6 +461,7 @@ def test_view_exists_204(rest_mock: Mocker) -> None:
     catalog = RestCatalog("rest", uri=TEST_URI, token=TEST_TOKEN)
     assert catalog.view_exists((namespace, view))
 
+
 def test_view_exists_404(rest_mock: Mocker) -> None:
     namespace = "examples"
     view = "some_view"
@@ -472,6 +473,7 @@ def test_view_exists_404(rest_mock: Mocker) -> None:
     catalog = RestCatalog("rest", uri=TEST_URI, token=TEST_TOKEN)
     assert not catalog.view_exists((namespace, view))
 
+
 def test_view_exists_multilevel_namespace_404(rest_mock: Mocker) -> None:
     multilevel_namespace = "core.examples.some_namespace"
     view = "some_view"
@@ -482,6 +484,7 @@ def test_view_exists_multilevel_namespace_404(rest_mock: Mocker) -> None:
     )
     catalog = RestCatalog("rest", uri=TEST_URI, token=TEST_TOKEN)
     assert not catalog.view_exists((multilevel_namespace, view))
+
 
 def test_list_namespaces_200(rest_mock: Mocker) -> None:
     rest_mock.get(
