12e87a4fb6cc7891a80fd18c9367bffd78255271
Mark Major <32452238+mark-major@users.noreply.github.com>
2024-11-20 09:26:30 +0100

    Boto Glue standard retry policy with configuration (#1307)
    * boto glue standard retry policy
    configurable max retry
    
    * Update configuration.md
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * Update glue.py
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * boto glue retry mode configurable
    
    ---------
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- pyiceberg/catalog/glue.py
+++ pyiceberg/catalog/glue.py
@@ -29,6 +29,7 @@ from typing import (
 )
 
 import boto3
+from botocore.config import Config
 from mypy_boto3_glue.client import GlueClient
 from mypy_boto3_glue.type_defs import (
     ColumnTypeDef,
@@ -128,6 +129,14 @@ GLUE_REGION = "glue.region"
 GLUE_ACCESS_KEY_ID = "glue.access-key-id"
 GLUE_SECRET_ACCESS_KEY = "glue.secret-access-key"
 GLUE_SESSION_TOKEN = "glue.session-token"
+GLUE_MAX_RETRIES = "glue.max-retries"
+GLUE_RETRY_MODE = "glue.retry-mode"
+
+MAX_RETRIES = 10
+STANDARD_RETRY_MODE = "standard"
+ADAPTIVE_RETRY_MODE = "adaptive"
+LEGACY_RETRY_MODE = "legacy"
+EXISTING_RETRY_MODES = [STANDARD_RETRY_MODE, ADAPTIVE_RETRY_MODE, LEGACY_RETRY_MODE]
 
 
 def _construct_parameters(
@@ -297,6 +306,8 @@ class GlueCatalog(MetastoreCatalog):
     def __init__(self, name: str, **properties: Any):
         super().__init__(name, **properties)
 
+        retry_mode_prop_value = get_first_property_value(properties, GLUE_RETRY_MODE)
+
         session = boto3.Session(
             profile_name=properties.get(GLUE_PROFILE_NAME),
             region_name=get_first_property_value(properties, GLUE_REGION, AWS_REGION),
@@ -305,7 +316,16 @@ class GlueCatalog(MetastoreCatalog):
             aws_secret_access_key=get_first_property_value(properties, GLUE_SECRET_ACCESS_KEY, AWS_SECRET_ACCESS_KEY),
             aws_session_token=get_first_property_value(properties, GLUE_SESSION_TOKEN, AWS_SESSION_TOKEN),
         )
-        self.glue: GlueClient = session.client("glue", endpoint_url=properties.get(GLUE_CATALOG_ENDPOINT))
+        self.glue: GlueClient = session.client(
+            "glue",
+            endpoint_url=properties.get(GLUE_CATALOG_ENDPOINT),
+            config=Config(
+                retries={
+                    "max_attempts": properties.get(GLUE_MAX_RETRIES, MAX_RETRIES),
+                    "mode": retry_mode_prop_value if retry_mode_prop_value in EXISTING_RETRY_MODES else STANDARD_RETRY_MODE,
+                }
+            ),
+        )
 
         if glue_catalog_id := properties.get(GLUE_ID):
             _register_glue_catalog_id_with_glue_client(self.glue, glue_catalog_id)
