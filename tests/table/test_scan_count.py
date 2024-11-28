da18837049d3a932e006dae0557ec44023e192b6
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-11-28 19:30:33 +0530

    implemented count in data scan

--- tests/table/test_scan_count.py
+++ tests/table/test_scan_count.py
@@ -1,6 +1,10 @@
 from pyiceberg.table import Table, TableMetadata, DataScan
 from pyiceberg.catalog.sql import SqlCatalog
 
+def get_plan(table: Table) -> DataScan:
+    return table.scan()
+
+
 def test_iceberg_count():
     table = _create_iceberg_metadata()
     assert len(table.to_arrow()) == 2
