349f1b5632a6916f055d3374e5d392a603f9ebd5
Sreesh Maheshwar <smaheshwar@palantir.com>
2024-12-21 04:56:42 +0000

    Add localhost driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -2254,6 +2254,7 @@ def spark() -> "SparkSession":
 
     spark = (
         SparkSession.builder.appName("PyIceberg integration test")
+        .config("spark.driver.host", "localhost")
         .config("spark.sql.session.timeZone", "UTC")
         .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
         .config("spark.sql.catalog.integration", "org.apache.iceberg.spark.SparkCatalog")
