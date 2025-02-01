4d141811e5a038936449bb7ef68f58f52520ea12
Borodin Gregory <grihabor@gmail.com>
2025-02-01 10:54:37 +0100

    Allow removing main branch

--- pyiceberg/table/update/__init__.py
+++ pyiceberg/table/update/__init__.py
@@ -474,13 +474,11 @@ def _(update: RemoveSnapshotRefUpdate, base_metadata: TableMetadata, context: _T
     if base_metadata.snapshot_by_id(existing_ref.snapshot_id) is None:
         raise ValueError(f"Cannot remove {update.ref_name} ref with unknown snapshot {existing_ref.snapshot_id}")
 
-    if update.ref_name == MAIN_BRANCH:
-        raise ValueError("Cannot remove main branch")
+    current_snapshot_id = None if update.ref_name == MAIN_BRANCH else base_metadata.current_snapshot_id
 
-    metadata_refs = {**base_metadata.refs}
-    metadata_refs.pop(update.ref_name, None)
+    metadata_refs = {ref_name: ref for ref_name, ref in base_metadata.refs.items() if ref_name != update.ref_name}
     context.add_update(update)
-    return base_metadata.model_copy(update={"refs": metadata_refs})
+    return base_metadata.model_copy(update={"refs": metadata_refs, "current_snapshot_id": current_snapshot_id})
 
 
 @_apply_table_update.register(AddSortOrderUpdate)
