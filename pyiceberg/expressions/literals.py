8e109c0296b72d74b194da8fcd3365601c0dcd64
TennyZhuang <zty0826@gmail.com>
2025-02-07 20:20:41 +0800

    Update pyiceberg/expressions/literals.py
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- pyiceberg/expressions/literals.py
+++ pyiceberg/expressions/literals.py
@@ -151,7 +151,7 @@ def literal(value: L) -> Literal[L]:
     elif isinstance(value, datetime):
         return TimestampLiteral(datetime_to_micros(value))  # type: ignore
     elif isinstance(value, date):
-        return DateLiteral(date_to_days(value))
+        return DateLiteral(date_to_days(value))  # type: ignore
     else:
         raise TypeError(f"Invalid literal value: {repr(value)}")
 
