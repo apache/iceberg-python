5e57697a9eafa5d76800d5b9f9cddb9a011a1d84
Jasper Ginn <jasperginn@gmail.com>
2025-02-16 15:26:20 +0100

    chore: fmt

--- pyiceberg/table/update/sorting.py
+++ pyiceberg/table/update/sorting.py
@@ -85,7 +85,9 @@ class UpdateSortOrder(UpdateTableMetadata["UpdateSortOrder"]):
             null_order=null_order,
         )
 
-    def desc(self, source_column_name: str, transform: Transform[Any, Any], null_order: NullOrder = NullOrder.NULLS_LAST) -> UpdateSortOrder:
+    def desc(
+        self, source_column_name: str, transform: Transform[Any, Any], null_order: NullOrder = NullOrder.NULLS_LAST
+    ) -> UpdateSortOrder:
         """Add a sort field with descending order."""
         return self._add_sort_field(
             source_id=self._column_name_to_id(source_column_name),
