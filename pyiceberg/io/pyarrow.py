9876cb436162c2961c18fa1f926311b007f499ef
Om Kenge <88768848+omkenge@users.noreply.github.com>
2025-02-22 14:26:33 +0000

    feat: rename pa to self

--- pyiceberg/io/pyarrow.py
+++ pyiceberg/io/pyarrow.py
@@ -613,8 +613,8 @@ class _ConvertToArrowSchema(SchemaVisitorPerPrimitiveType[pa.DataType]):
         return pa.large_list(value_type=element_field)
 
     def map(self, map_type: MapType, key_result: pa.DataType, value_result: pa.DataType) -> pa.DataType:
-        key_field = pa.field("key", key_result, nullable=map_type.key_field.optional)
-        value_field = pa.field("value", value_result, nullable=map_type.value_field.optional)
+        key_field = self.field("key", key_result, nullable=map_type.key_field.optional)
+        value_field = self.field("value", value_result, nullable=map_type.value_field.optional)
         return pa.map_(key_type=key_field, item_type=value_field)
 
     def visit_fixed(self, fixed_type: FixedType) -> pa.DataType:
