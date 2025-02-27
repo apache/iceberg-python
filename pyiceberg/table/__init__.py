62b62dc180138e37545231498800b268fe2935f8
Andor Markus <andor.markus@allcloud.io>
2025-02-27 20:35:30 +0100

    feat: Add write_parquet method to write files without committing
    This method allows users to write a PyArrow table to the table's storage format
    as Parquet files without committing them to the table. The method returns a list
    of file paths that were written, enabling workflows that require access to the
    data files before committing metadata changes.
    
    Also adds an include_field_ids parameter to the underlying write_file and
    _dataframe_to_data_files functions to provide more control over the Parquet
    writing process.

--- pyiceberg/table/__init__.py
+++ pyiceberg/table/__init__.py
@@ -1262,7 +1262,7 @@ class Table:
         if df.shape[0] > 0:
             data_files = list(
                 _dataframe_to_data_files(
-                    table_metadata=self.metadata, write_uuid=uuid.uuid4(), df=df, io=self.io
+                    table_metadata=self.metadata, write_uuid=uuid.uuid4(), df=df, io=self.io, include_field_ids=False
                 )
             )
 
