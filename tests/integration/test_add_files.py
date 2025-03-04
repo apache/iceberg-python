fff741490d97c5c406b30cbb5b7b8ffd192c73bf
Fokko <fokko@apache.org>
2025-03-04 10:20:07 +0100

    Fix import

--- tests/integration/test_add_files.py
+++ tests/integration/test_add_files.py
@@ -33,7 +33,7 @@ from pytest_mock.plugin import MockerFixture
 from pyiceberg.catalog import Catalog
 from pyiceberg.exceptions import NoSuchTableError
 from pyiceberg.io import FileIO
-from pyiceberg.io.pyarrow import UnsupportedPyArrowTypeException, _pyarrow_schema_ensure_large_types
+from pyiceberg.io.pyarrow import UnsupportedPyArrowTypeException
 from pyiceberg.manifest import DataFile
 from pyiceberg.partitioning import UNPARTITIONED_PARTITION_SPEC, PartitionField, PartitionSpec
 from pyiceberg.schema import Schema
