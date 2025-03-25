6cb546a6a4a1a7a68cafd35285bb575f5a7b94d0
Fokko <fokko@apache.org>
2025-03-25 20:37:52 +0100

    fix some tests

--- tests/integration/test_rest_manifest.py
+++ tests/integration/test_rest_manifest.py
@@ -20,7 +20,7 @@ import inspect
 from copy import copy
 from enum import Enum
 from tempfile import TemporaryDirectory
-from typing import Any
+from typing import Any, List
 
 import pytest
 from fastavro import reader
@@ -29,12 +29,15 @@ from pyiceberg.catalog import Catalog, load_catalog
 from pyiceberg.io.pyarrow import PyArrowFileIO
 from pyiceberg.manifest import DataFile, write_manifest
 from pyiceberg.table import Table
+from pyiceberg.typedef import Record
 from pyiceberg.utils.lazydict import LazyDict
 
 
 # helper function to serialize our objects to dicts to enable
 # direct comparison with the dicts returned by fastavro
-def todict(obj: Any) -> Any:
+def todict(obj: Any, spec_keys: List[str]) -> Any:
+    if type(obj) is Record:
+        return {key: obj[pos] for key, pos in zip(spec_keys, range(len(obj)))}
     if isinstance(obj, dict) or isinstance(obj, LazyDict):
         data = []
         for k, v in obj.items():
@@ -43,9 +46,13 @@ def todict(obj: Any) -> Any:
     elif isinstance(obj, Enum):
         return obj.value
     elif hasattr(obj, "__iter__") and not isinstance(obj, str) and not isinstance(obj, bytes):
-        return [todict(v) for v in obj]
+        return [todict(v, spec_keys) for v in obj]
     elif hasattr(obj, "__dict__"):
-        return {key: todict(value) for key, value in inspect.getmembers(obj) if not callable(value) and not key.startswith("_")}
+        return {
+            key: todict(value, spec_keys)
+            for key, value in inspect.getmembers(obj)
+            if not callable(value) and not key.startswith("_")
+        }
     else:
         return obj
 
@@ -102,7 +109,7 @@ def test_write_sample_manifest(table_test_all_types: Table) -> None:
     )
     wrapped_entry_v2 = copy(entry)
     wrapped_entry_v2.data_file = wrapped_data_file_v2_debug
-    wrapped_entry_v2_dict = todict(wrapped_entry_v2)
+    wrapped_entry_v2_dict = todict(wrapped_entry_v2, [field.name for field in test_spec.fields])
 
     with TemporaryDirectory() as tmpdir:
         tmp_avro_file = tmpdir + "/test_write_manifest.avro"
