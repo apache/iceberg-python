562de407d0dbf5e1212f0a541001a12543a645f3
Fokko Driesprong <fokko@apache.org>
2025-04-16 20:35:36 +0200

    Fix `add_files` with non-identity transforms (#1925)
    <!--
    Thanks for opening a pull request!
    -->
    
    <!-- In the case this PR will resolve an issue, please replace
    ${GITHUB_ISSUE_ID} below with the actual Github issue id. -->
    <!-- Closes #${GITHUB_ISSUE_ID} -->
    
    Found out I broke this myself after doing a `git bisect`:
    
    ```
    36d383dcb676ae5ef59c34cc2910d16a8e30a80c is the first bad commit
    commit 36d383dcb676ae5ef59c34cc2910d16a8e30a80c
    Author: Fokko Driesprong <fokko@apache.org>
    Date:   Thu Jan 23 07:50:54 2025 +0100
    
        PyArrow: Avoid buffer-overflow by avoid doing a sort (#1555)
    
        Second attempt of https://github.com/apache/iceberg-python/pull/1539
    
        This was already being discussed back here:
        https://github.com/apache/iceberg-python/issues/208#issuecomment-1889891973
    
        This PR changes from doing a sort, and then a single pass over the table
        to the approach where we determine the unique partition tuples filter on
        them individually.
    
        Fixes https://github.com/apache/iceberg-python/issues/1491
    
        Because the sort caused buffers to be joined where it would overflow in
        Arrow. I think this is an issue on the Arrow side, and it should
        automatically break up into smaller buffers. The `combine_chunks` method
        does this correctly.
    
        Now:
    
        ```
        0.42877754200890195
        Run 1 took: 0.2507691659993725
        Run 2 took: 0.24833179199777078
        Run 3 took: 0.24401691700040828
        Run 4 took: 0.2419595829996979
        Average runtime of 0.28 seconds
        ```
    
        Before:
    
        ```
        Run 0 took: 1.0768639159941813
        Run 1 took: 0.8784021250030492
        Run 2 took: 0.8486490420036716
        Run 3 took: 0.8614017910003895
        Run 4 took: 0.8497851670108503
        Average runtime of 0.9 seconds
        ```
    
        So it comes with a nice speedup as well :)
    
        ---------
    
        Co-authored-by: Kevin Liu <kevinjqliu@users.noreply.github.com>
    
     pyiceberg/io/pyarrow.py                    |  129 ++-
     pyiceberg/partitioning.py                  |   39 +-
     pyiceberg/table/__init__.py                |    6 +-
     pyproject.toml                             |    1 +
     tests/benchmark/test_benchmark.py          |   72 ++
     tests/integration/test_partitioning_key.py | 1299 ++++++++++++++--------------
     tests/table/test_locations.py              |    2 +-
     7 files changed, 805 insertions(+), 743 deletions(-)
     create mode 100644 tests/benchmark/test_benchmark.py
    ```
    
    Closes #1917
    
    <!-- In the case of user-facing changes, please add the changelog label.
    -->

--- pyiceberg/io/pyarrow.py
+++ pyiceberg/io/pyarrow.py
@@ -2205,29 +2205,36 @@ class DataFileStatistics:
         if partition_field.source_id not in self.column_aggregates:
             return None
 
-        if not partition_field.transform.preserves_order:
+        source_field = schema.find_field(partition_field.source_id)
+        iceberg_transform = partition_field.transform
+
+        if not iceberg_transform.preserves_order:
             raise ValueError(
                 f"Cannot infer partition value from parquet metadata for a non-linear Partition Field: {partition_field.name} with transform {partition_field.transform}"
             )
 
-        lower_value = partition_record_value(
-            partition_field=partition_field,
-            value=self.column_aggregates[partition_field.source_id].current_min,
-            schema=schema,
+        transform_func = iceberg_transform.transform(source_field.field_type)
+
+        lower_value = transform_func(
+            partition_record_value(
+                partition_field=partition_field,
+                value=self.column_aggregates[partition_field.source_id].current_min,
+                schema=schema,
+            )
         )
-        upper_value = partition_record_value(
-            partition_field=partition_field,
-            value=self.column_aggregates[partition_field.source_id].current_max,
-            schema=schema,
+        upper_value = transform_func(
+            partition_record_value(
+                partition_field=partition_field,
+                value=self.column_aggregates[partition_field.source_id].current_max,
+                schema=schema,
+            )
         )
         if lower_value != upper_value:
             raise ValueError(
                 f"Cannot infer partition value from parquet metadata as there are more than one partition values for Partition Field: {partition_field.name}. {lower_value=}, {upper_value=}"
             )
 
-        source_field = schema.find_field(partition_field.source_id)
-        transform = partition_field.transform.transform(source_field.field_type)
-        return transform(lower_value)
+        return lower_value
 
     def partition(self, partition_spec: PartitionSpec, schema: Schema) -> Record:
         return Record(**{field.name: self._partition_value(field, schema) for field in partition_spec.fields})
