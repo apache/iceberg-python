2a1bf5c5ecffe40f9d38b7fbda93f070dad37a29
DinGo4DEV <stanleylkal@gmail.com>
2025-05-14 16:37:53 +0800

    align return type with `Union`

--- pyiceberg/partitioning.py
+++ pyiceberg/partitioning.py
@@ -467,7 +467,7 @@ def _(type: IcebergType, value: Optional[time]) -> Optional[int]:
 
 
 @_to_partition_representation.register(UUIDType)
-def _(type: IcebergType, value: Optional[Union[uuid.UUID,int]]) -> Optional[str|int]:
+def _(type: IcebergType, value: Optional[Union[uuid.UUID,int]]) -> Optional[Union[str,int]]:
     if value is None: 
         return None
     if isinstance(value, uuid.UUID):
