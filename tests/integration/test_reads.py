5c4b3b47594e1586049c6041dfe5e8afaa2a4f08
smaheshwar-pltr <maheshwarsreesh@gmail.com>
2025-07-18 21:53:34 +0100

    Fix `TableScan.update` to exclude cached properties (#2178)
    <!--
    Thanks for opening a pull request!
    -->
    
    <!-- In the case this PR will resolve an issue, please replace
    ${GITHUB_ISSUE_ID} below with the actual Github issue id. -->
    <!-- Closes #${GITHUB_ISSUE_ID} -->
    
    # Rationale for this change
    
    Closes https://github.com/apache/iceberg-python/issues/2179.
    
    # Are these changes tested?
    
    Yes.
    
    # Are there any user-facing changes?
    
    Yes, the scenario shown in the test and described in the issue now
    works.
    
    <!-- In the case of user-facing changes, please add the changelog label.
    -->
    
    ---------
    
    Co-authored-by: Sreesh Maheshwar <smaheshwar@palantir.com>

--- tests/integration/test_reads.py
+++ tests/integration/test_reads.py
@@ -1057,3 +1057,16 @@ def test_initial_default(catalog: Catalog, spark: SparkSession) -> None:
     result_table = tbl.scan().filter("so_true == True").to_arrow()
 
     assert len(result_table) == 10
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("catalog", [pytest.lazy_fixture("session_catalog_hive"), pytest.lazy_fixture("session_catalog")])
+def test_filter_after_arrow_scan(catalog: Catalog) -> None:
+    identifier = "test_partitioned_by_hours"
+    table = catalog.load_table(f"default.{identifier}")
+
+    scan = table.scan()
+    assert len(scan.to_arrow()) > 0
+
+    scan = scan.filter("ts >= '2023-03-05T00:00:00+00:00'")
+    assert len(scan.to_arrow()) > 0
