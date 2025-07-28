cee4017fee525bfca95e26f74f630aaedb1dfd7b
ForeverAngry <61765732+ForeverAngry@users.noreply.github.com>
2025-07-27 20:04:55 -0400

    Fixed linting issues for the CI/CD Process

--- pyiceberg/table/inspect.py
+++ pyiceberg/table/inspect.py
@@ -17,7 +17,6 @@
 from __future__ import annotations
 
 from datetime import datetime, timezone
-from functools import reduce
 from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Optional, Set, Tuple
 
 from pyiceberg.conversions import from_bytes
