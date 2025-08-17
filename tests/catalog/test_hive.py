dff6a489035499cb5d6794a1e0dc9ec18068f5fe
Gabriel Igliozzi <gaboiglio@gmail.com>
2025-08-17 03:05:22 -0700

    add assertion for error message

--- tests/catalog/test_hive.py
+++ tests/catalog/test_hive.py
@@ -976,9 +976,10 @@ def test_rename_table_to_table_already_exists(hive_table: HiveTable) -> None:
     catalog = HiveCatalog(HIVE_CATALOG_NAME, uri=HIVE_METASTORE_FAKE_URL)
     catalog.load_table = MagicMock(return_value=hive_table)  # type: ignore[method-assign]
 
-    with pytest.raises(TableAlreadyExistsError):
+    with pytest.raises(TableAlreadyExistsError) as exc_info:
         catalog.rename_table(("default", "some_table"), ("default", "new_tabl2e"))
 
+    assert "Table already exists: new_tabl2e" in str(exc_info.value)
 
 def test_drop_database_does_not_empty() -> None:
     catalog = HiveCatalog(HIVE_CATALOG_NAME, uri=HIVE_METASTORE_FAKE_URL)
