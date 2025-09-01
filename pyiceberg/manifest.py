aa0ef95ef1e26f20ae1bf545a143f94934283ff7
Fokko Driesprong <fokko@apache.org>
2025-09-01 23:50:54 +0200

    Update manifest.py
    Co-authored-by: Kevin Liu <kevinjqliu@users.noreply.github.com>

--- pyiceberg/manifest.py
+++ pyiceberg/manifest.py
@@ -1091,7 +1091,7 @@ class ManifestWriter(ABC):
             ManifestEntry.from_args(
                 status=ManifestEntryStatus.ADDED,
                 snapshot_id=self._snapshot_id,
-                sequence_number=entry.sequence_number if entry.file_sequence_number != UNASSIGNED_SEQ else None,
+                sequence_number=entry.sequence_number if entry.sequence_number != UNASSIGNED_SEQ else None,
                 data_file=entry.data_file,
             )
         )
