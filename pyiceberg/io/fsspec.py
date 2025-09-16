6e018c85af73bbc0250620e8d046bc08f9dfb121
Thomas Powell <tom-s-powell@hotmail.co.uk>
2025-09-16 20:25:00 +0100

    Fix prefix match for ADLS_SAS_TOKEN (#2461)
    The current logic when constructing an ADLS file system incorrectly
    matches
    [`adls.sas-token-expires-at-ms.`](https://github.com/apache/iceberg/blob/d1771207c9040f1c8b6886665b56d1a972fe402a/azure/src/main/java/org/apache/iceberg/azure/AzureProperties.java#L45)
    prefixed keys. If this is provided and encountered prior to
    [`adls.sas-token.`](https://github.com/apache/iceberg/blob/d1771207c9040f1c8b6886665b56d1a972fe402a/azure/src/main/java/org/apache/iceberg/azure/AzureProperties.java#L44)
    prefixed keys, it will incorrectly determine the `ADLS_ACCOUNT_NAME` and
    `ADLS_SAS_TOKEN`.
    
    For example with the following properties:
    ```
    {
        'adls.sas-token-expires-at-ms.testaccount.dfs.core.windows.net': '1757597218121',
        'adls.sas-token.testaccount.dfs.core.windows.net': '<redacted>'
    }
    ```
    
    **Before**
    ```
    {
        'adls.sas-token-expires-at-ms.testaccount.dfs.core.windows.net': '1757597218121',
        'adls.sas-token.testaccount.dfs.core.windows.net': '<redacted>',
        'adls.account-name': 'adls',
        'adls.sas-token': '1757597218121'
    }
    ```
    
    **After**
    ```
    {
        'adls.sas-token-expires-at-ms.testaccount.dfs.core.windows.net': '1757597218121',
        'adls.sas-token.testaccount.dfs.core.windows.net': '<redacted>',
        'adls.account-name': 'testaccount',
        'adls.sas-token': '<redacted>'
    }
    ```
    
    ---------
    
    Co-authored-by: Thomas Powell <tpowell@palantir.com>

--- pyiceberg/io/fsspec.py
+++ pyiceberg/io/fsspec.py
@@ -207,7 +207,7 @@ def _adls(properties: Properties) -> AbstractFileSystem:
     from azure.core.credentials_async import AsyncTokenCredential
 
     for key, sas_token in {
-        key.replace(f"{ADLS_SAS_TOKEN}.", ""): value for key, value in properties.items() if key.startswith(ADLS_SAS_TOKEN)
+        key.replace(f"{ADLS_SAS_TOKEN}.", ""): value for key, value in properties.items() if key.startswith(f"{ADLS_SAS_TOKEN}.")
     }.items():
         if ADLS_ACCOUNT_NAME not in properties:
             properties[ADLS_ACCOUNT_NAME] = key.split(".")[0]
