5ed46a82b4550a954abe9477f88458dcc49dc886
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-10-02 12:36:05 +0530

    Update pyiceberg/expressions/__init__.py
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- pyiceberg/expressions/__init__.py
+++ pyiceberg/expressions/__init__.py
@@ -564,7 +564,7 @@ class NotNaN(UnaryPredicate):
 class SetPredicate(UnboundPredicate[L], IcebergBaseModel, ABC):
     type: str = Field(default="in", alias="type")
     term: str
-    value: list[Any]
+    literals: Set[Literal[L]] = Field(alias="items")
 
     def __init__(self, term: Union[str, UnboundTerm[Any]], literals: Union[Iterable[L], Iterable[Literal[L]]]):
         # Convert term to string for serialization
