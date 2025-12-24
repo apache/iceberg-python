c6e6420a85be6a67ab14035a4c70f4ca93d21fac
Kevin Liu <kevin.jq.liu@gmail.com>
2025-12-24 14:16:25 -0800

    fix ray local init

--- tests/conftest.py
+++ tests/conftest.py
@@ -2947,3 +2947,16 @@ def pyarrow_table_with_promoted_types(pyarrow_schema_with_promoted_types: "pa.Sc
         },
         schema=pyarrow_schema_with_promoted_types,
     )
+
+
+@pytest.fixture(scope="function")
+def ray_session() -> Generator[Any, None, None]:
+    """Fixture to manage Ray initialization and shutdown for tests."""
+    import ray
+
+    ray.init(
+        ignore_reinit_error=True,
+        runtime_env={"working_dir": None},  # Prevent Ray from serializing the working directory to workers
+    )
+    yield ray
+    ray.shutdown()
