2990256fcc894f516b47d4b2f5d487b8444cbf52
Junwang Zhao <zhjwpku@gmail.com>
2026-06-17 00:18:05 +0800

    Reject unsupported identity transform types

--- pyiceberg/transforms.py
+++ pyiceberg/transforms.py
@@ -73,6 +73,8 @@ from pyiceberg.types import (
     DateType,
     DecimalType,
     FixedType,
+    GeographyType,
+    GeometryType,
     IcebergType,
     IntegerType,
     LongType,
@@ -717,7 +719,7 @@ class IdentityTransform(Transform[S, S]):
         return lambda v: v
 
     def can_transform(self, source: IcebergType) -> bool:
-        return source.is_primitive
+        return source.is_primitive and not isinstance(source, (GeographyType, GeometryType))
 
     def result_type(self, source: IcebergType) -> IcebergType:
         return source
