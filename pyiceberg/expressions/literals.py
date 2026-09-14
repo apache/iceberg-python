03d3610479e850c319eacf6fac404d7fe1316e6a
Alex Stephen <alexstephen@google.com>
2026-09-14 20:59:46 +0000

    make lint was complaining

--- pyiceberg/expressions/literals.py
+++ pyiceberg/expressions/literals.py
@@ -175,7 +175,7 @@ def literal(value: L) -> Literal[L]:
 
 class AboveMax(Literal[L]):
     def __init__(self, value: L, value_type: type[L], /, **data: Any) -> None:
-        super().__init__(value, value_type, **data)
+        Literal.__init__(self, value, value_type, **data)
 
     def __repr__(self) -> str:
         """Return the string representation of the AboveMax class."""
@@ -188,7 +188,7 @@ class AboveMax(Literal[L]):
 
 class BelowMin(Literal[L]):
     def __init__(self, value: L, value_type: type[L], /, **data: Any) -> None:
-        super().__init__(value, value_type, **data)
+        Literal.__init__(self, value, value_type, **data)
 
     def __repr__(self) -> str:
         """Return the string representation of the BelowMin class."""
